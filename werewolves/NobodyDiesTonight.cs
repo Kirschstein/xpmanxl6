2660b5d03b91c864f41a8bf9682a9c2f120d8e8a
unknown <themrtortoise@gmail.com>
2018-10-13 11:53:31 +0100

    automatically commit passing test

---
+++ werewolves/NobodyDiesTonight.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace werewolves
+{
+    public class NobodyDiesTonightSpec
+    { 
+        [Fact]
+        public void NobodyDiesTonight()
+        {
+            Assert.True(true);
+
+        }
+    }
+}
