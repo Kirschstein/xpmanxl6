a76343eeac9219429ca07846bf997d59c887067e
unknown <themrtortoise@gmail.com>
2018-10-13 14:05:23 +0100

    automatically commit passing test

--- werewolves/NobodyDiesTonight.cs
+++ werewolves/NobodyDiesTonight.cs
@@ -20,20 +20,15 @@ namespace werewolves
         [Fact]
         public void WerewolfTargetsFred()
         {
-            SendOrder("target Fred");
+            _moderator.SendOrder("target Fred");
             Assert.Equal("Fred has died", LastMessage());
         }
 
         [Fact]
         public void SueDies()
         {
-            SendOrder("target Sue");
+            _moderator.SendOrder("target Sue");
             Assert.Equal("Sue has died", LastMessage());
-        }
-
-        public void SendOrder(string order)
-        {
-            _moderator.SendOrder(order);
         }
 
         private string LastMessage()
