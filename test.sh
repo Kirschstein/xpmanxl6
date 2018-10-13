746b606be872dc226b55080eb378dfab6632d7c7
Paul Wild <paulwild@gmail.com>
2018-10-13 11:09:42 +0100

    updated test script

--- test.sh
+++ test.sh
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 nunit3-console C/src/xpmanxl6/Tests/bin/Debug/netcoreapp2.1/Tests.dll
+=======
+dotnet test werewolves/
+>>>>>>> updated test script
 if [ $? -eq 0 ]
 then
     git add .
