14323ad7d56e6a5a7d46b43d18e6d28137b445c6
unknown <themrtortoise@gmail.com>
2018-10-13 12:08:16 +0100

    automatically commit passing test

--- werewolves/NobodyDiesTonight.cs
+++ werewolves/NobodyDiesTonight.cs
@@ -6,12 +6,30 @@ using Xunit;
 namespace werewolves
 {
     public class NobodyDiesTonightSpec
-    { 
+    {
+        private string _message = "it is now daytime";
+
         [Fact]
         public void NobodyDiesTonight()
         {
-            Assert.True(true);
+            Assert.Equal("it is now daytime", LastMessage());
+        }
 
+        [Fact]
+        public void OnePersonDies()
+        {
+            SendOrder();
+            Assert.Equal("Fred has died", LastMessage());
+        }
+        
+        private void SendOrder()
+        {
+            _message = "Fred has died";
+        }
+
+        private string LastMessage()
+        {
+            return _message;
         }
     }
 }
